7c01e473068008713b76d38875520b7ea16d73df
Nathan Baldez <nathanbsb2@hotmail.com>
2021-07-20 21:08:47 -0300

    first commit

---
+++ bin2dec/scripts.js
@@ -0,0 +1,14 @@
+const inp = document.querySelector("#binary");
+const dec = document.querySelector(".decimal");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener('click', () => {
+
+    let bin = inp.value;
+    let num = parseInt((bin + '').replace(/[^01]/gi, ''), 2);
+    
+    dec.innerHTML = `<div>${num}</div>`;
+    
+    
+
+});
